f5cccaed7619dbf4e84fc830352fa6af22a16f5c
SebasShogun <shogun31sff@gmail.com>
2020-05-23 14:46:49 -0500

    Vistas de Blog

--- core/views.py
+++ core/views.py
@@ -23,9 +23,6 @@ def store(request):
 def contact(request):
     return render(request, "core/contact.html")
 
-def blog(request):
-    return render(request, "core/blog.html")
-
 def sample(request):
     return render(request, "core/sample.html")
 
