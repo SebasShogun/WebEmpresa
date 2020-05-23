2cd7c22c7597e0a8920b9dbda19d545433ad4300
SebasShogun <shogun31sff@gmail.com>
2020-05-23 02:08:35 -0500

    Fin de la primera parte

--- blog/admin.py
+++ blog/admin.py
@@ -7,6 +7,15 @@ class CategoryAdmin(admin.ModelAdmin):
 
 class PostAdmin(admin.ModelAdmin):
     readonly_fields = ('created', 'updated')
+    list_display = ('title', 'author', 'published','post_categories')
+    ordering = ('author', 'published')
+    search_fields = ('title','content','author__username', 'categories__name')
+    date_hierarchy = 'published'
+    list_filter = ('author__username','categories__name')
+
+    def post_categories(self, obj):
+        return ", ".join([c.name for c in obj.categories.all().order_by("name")])
+    post_categories.short_description = "Categorías"
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Post, PostAdmin)
--- core/views.py
+++ core/views.py
@@ -17,9 +17,6 @@ def home(request):
 def about(request):
     return render(request, "core/about.html")
 
-def services(request):
-    return render(request, "core/services.html")
-
 def store(request):
     return render(request, "core/store.html")
 
